c0f6c737101933c07d4ec01cdbcee166bc789a78
flopt04 <zakusenshi@gmail.com>
2023-10-02 13:45:50 +0100

    stuff

--- src/components/Pages/Contacts/Faq_u.js
+++ src/components/Pages/Contacts/Faq_u.js
@@ -60,14 +60,7 @@ export default function Faq() {
                   <p class="text-gray-500 dark:text-gray-400">{t("faq_b_p_9")} </p>
                   <p class="text-gray-500 dark:text-gray-400">{t("faq_b_p_10")}<Link to={"/contactus"} class="font-medium underline text-terciary-yellowish dark:text-terciary-yellowish hover:no-underline" rel="">{t("faq_b_p_10.1")}</Link> {t("faq_b_p_10.2")}</p>
               </div>
-              <div class="mb-10">
-                  <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-                      <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                      What is the difference between Windster and Tailwind UI?
-                  </h3>
-                  <p class="text-gray-500 dark:text-gray-400">Although both Windster and Tailwind UI are built for integration with Tailwind CSS, the main difference is in the design, the pages, the extra components and UI elements that Windster includes.</p>
-                  <p class="text-gray-500 dark:text-gray-400">Additionally, Windster is a project that is still in development, and later it will include both the application, marketing, and e-commerce UI interfaces.</p>
-              </div>
+              
           </div>
       </div>
   </div>
