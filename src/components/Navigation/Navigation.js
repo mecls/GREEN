3f72264f638cb2f76411002bf5225d257bf144c8
mecls <carvalhal.busin@gmail.com>
2023-10-05 18:54:55 +0100

    navigation mobile resposivness

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -231,50 +231,29 @@ export default function Navigation() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        {t("nav_seller")}
-                        <ChevronDownIcon
-                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
-                          aria-hidden="true"
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
                 <NavLink
                  to={"whygreen"}
+                 onClick={() => setMobileMenuOpen(false)}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                    {t("nav_whygreen")}
                 </NavLink>
                 <NavLink
-                  to={"marketplace"}
+                  to={"faq_u"}
+                  onClick={() => setMobileMenuOpen(false)}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Marketplace
+                  FAQ
                 </NavLink>
                 <NavLink
                   to={"contactus"}
+                  onClick={() => setMobileMenuOpen(false)}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                    {t("nav_contact")}
                 </NavLink>
-                <Disclosure as="div" className="-mx-3">
+                <Disclosure as="div" 
+                 className="-mx-3">
                   {({ open }) => (
                     <>
                       <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
@@ -291,7 +270,9 @@ export default function Navigation() {
                             as="a"
                             onClick={() => {
                               i18n.changeLanguage(item.code)
+                              setMobileMenuOpen(false)
                             }}
+
                             className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                           >
                             {item.name}
@@ -304,12 +285,13 @@ export default function Navigation() {
               </div>
 
               <div className="py-6">
-              <a
-                  href="#00"
+              <NavLink
+                  to={"login"}
+                  onClick={() => setMobileMenuOpen(false)}
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in
-                </a>
+                </NavLink>
               </div>
             </div>
           </div>
