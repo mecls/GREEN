45dcb78cc8c25617a221ce6c78b31b2fa655d430
flopt04 <zakusenshi@gmail.com>
2023-10-08 15:24:07 +0100

    linked in llinks

--- src/components/Pages/HomePage/Footer/Footer.js
+++ src/components/Pages/HomePage/Footer/Footer.js
@@ -55,7 +55,11 @@ export default function Footer() {
                         <li className="mb-4 text-xl">
                             <a href="https://www.instagram.com/vileaf" title="link for vileaf instagram" rel="noreferrer"  target="_blank" className="text-body-color hover:underline">{t("2_footer_a")}</a>
                         </li>
+                        <li className="mb-4 text-xl">
+                            <a href="https://www.linkedin.com/company/vileaf" title="link for vileaf linkedin" rel="noreferrer"  target="_blank" className="text-body-color hover:underline">LinkedIn</a>
+                        </li>
                     </ul>
+                    
                 </div>
                 <div>
                     <h2 className="mb-6 text-xl font-semibold text-gray-900 uppercase dark:text-white">{t("3_footer_h2")}</h2>
@@ -81,8 +85,8 @@ export default function Footer() {
                 <a href="https://twitter.com/vileaf1" rel="noreferrer" title="link for vileaf twitter" target="_blank" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                     <svg className="w-5 h-5" fill="#000000" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                 </a>
-             
-              
+                <a href="https://www.linkedin.com/company/vileaf" rel="noreferrer" title="link for vileaf linkedin" target="_blank" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-5 h-5" aria-hidden="true" fill="#000000" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>                </a>
             </div>
         </div>
     </div>
