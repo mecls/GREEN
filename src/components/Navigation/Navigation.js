6dc4f0e3f501a724874e0ec0645aaa34846c4e29
mecls <carvalhal.busin@gmail.com>
2023-10-16 15:22:41 +0100

    link added to mobile logo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -216,10 +216,10 @@ export default function Navigation() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#00" className="-m-1.5 p-1.5">
+            <Link to={"/"} className="-m-1.5 p-1.5">
               <span className="sr-only">Green</span>
            <Logo/>
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
